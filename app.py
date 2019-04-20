29f373745c82f5fe7918691489e412cf46c0deaa
Aasif Multani <aasif_6944@yahoo.com>
2019-04-20 20:16:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from keras.models import load_model
 
 import nltk
 nltk.download('stopwords')
+nltk.download('punkt')
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
