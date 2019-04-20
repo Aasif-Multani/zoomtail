fb6ebf704f73de59480a26d104a5d3bbbda16801
Aasif Multani <aasif_6944@yahoo.com>
2019-04-20 20:13:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import load_model
 
+import nltk
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
